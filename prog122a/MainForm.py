79d7265506a867c946dabe2093140f6cd59e5652
jimmyarneson <arneson.j1@stu.janesville.k12.wi.us>
2024-02-02 14:36:27 -0600

    2/2/24

--- prog122a/MainForm.py
+++ prog122a/MainForm.py
@@ -21,7 +21,7 @@ class MainForm(Form):
 		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
 		self._button1.Location = System.Drawing.Point(12, 407)
 		self._button1.Name = "button1"
-		self._button1.Size = System.Drawing.Size(86, 36)
+		self._button1.Size = System.Drawing.Size(168, 36)
 		self._button1.TabIndex = 0
 		self._button1.Text = "Calculate"
 		self._button1.UseVisualStyleBackColor = True
@@ -30,22 +30,24 @@ class MainForm(Form):
 		# button2
 		# 
 		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button2.Location = System.Drawing.Point(115, 407)
+		self._button2.Location = System.Drawing.Point(186, 407)
 		self._button2.Name = "button2"
-		self._button2.Size = System.Drawing.Size(86, 36)
+		self._button2.Size = System.Drawing.Size(198, 36)
 		self._button2.TabIndex = 1
 		self._button2.Text = "Clear"
 		self._button2.UseVisualStyleBackColor = True
+		self._button2.Click += self.Button2Click
 		# 
 		# button3
 		# 
 		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 12, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-		self._button3.Location = System.Drawing.Point(219, 407)
+		self._button3.Location = System.Drawing.Point(390, 407)
 		self._button3.Name = "button3"
-		self._button3.Size = System.Drawing.Size(86, 36)
+		self._button3.Size = System.Drawing.Size(131, 36)
 		self._button3.TabIndex = 2
 		self._button3.Text = "Exit"
 		self._button3.UseVisualStyleBackColor = True
+		self._button3.Click += self.Button3Click
 		# 
 		# listBox1
 		# 
@@ -54,12 +56,12 @@ class MainForm(Form):
 		self._listBox1.ItemHeight = 24
 		self._listBox1.Location = System.Drawing.Point(12, 13)
 		self._listBox1.Name = "listBox1"
-		self._listBox1.Size = System.Drawing.Size(293, 388)
+		self._listBox1.Size = System.Drawing.Size(509, 388)
 		self._listBox1.TabIndex = 3
 		# 
 		# MainForm
 		# 
-		self.ClientSize = System.Drawing.Size(317, 455)
+		self.ClientSize = System.Drawing.Size(533, 455)
 		self.Controls.Add(self._listBox1)
 		self.Controls.Add(self._button3)
 		self.Controls.Add(self._button2)
@@ -70,7 +72,13 @@ class MainForm(Form):
 
 
 	def Button1Click(self, sender, e):
-		self.listBox1.Text.Clear()
-		
-		for num in range(1,51)
-		return num ** 
+		self._listBox1.Items.Clear()
+		self._listBox1.Items.Add("Number \t   Square \t   Square Root")
+		for num in range(1,51):
+			self._listBox1.Items.Add(str(num)+"\t     "+(str(num**2.0)+"\t    "+(str(num**0.50))))
+
+	def Button2Click(self, sender, e):
+		self._listBox1.Items.Clear()
+
+	def Button3Click(self, sender, e):
+		Application.Exit()

28c4971e31e115b53e08656244fc7bce9baddc0f
jimmyarneson <143823450+jimmyarneson@users.noreply.github.com>
2024-02-07 20:32:51 +0000

    2/7/24

--- lp4-5.py
+++ lp4-5.py
@@ -1,9 +1,10 @@
 import random
-playernum = int(input("Enter a number 1-20: "))
 computernum = random.randint(1, 20)
-
+playernum = int(input("Enter a number 1-20: "))
 print("Computer Number: " + str(computernum))
 print("Your Number: " + str(playernum))
+  
+
 
 if computernum == playernum:
   print(str("You Win!!!"))

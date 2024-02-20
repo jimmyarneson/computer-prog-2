068b455c3cd6d7114142750e4c03f0a1e11189f0
jimmyarneson <143823450+jimmyarneson@users.noreply.github.com>
2024-02-20 21:25:30 +0000

    2/20/24

--- prog54cclass.py
+++ prog54cclass.py
@@ -6,6 +6,16 @@ class fortnite:
     self.circufrence = 0
 
   def calculate(self):
+    self.radius * 3.14 ** 2
+    3.14 * 2 ** self.radius
+
+  def radius(radius):
+    print(float(st))
     
-    
-  
+  def main():
+
+    pass
+
+
+  if __name__ == "__main__":
+    main()

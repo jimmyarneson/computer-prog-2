6ecf3ccb99ef019cf6c509e800b6b11c23dcbed7
jimmyarneson <143823450+jimmyarneson@users.noreply.github.com>
2024-02-22 20:05:13 +0000

    2/22/24

--- prog54cclass.py
+++ prog54cclass.py
@@ -3,30 +3,27 @@ class fortnite:
   def __init__(self, radius):
     self.radius = radius
     self.area = 0
-    self.circufrence = 0
+    self.cir = 0
 
   def calculate(self):
-    self.radius * 3.14 ** 2
-    3.14 * 2 ** self.radius
+    self.area = self.radius * 3.14 ** 2
+    self.cir = 3.14 * 2 ** self.radius
 
-  def radius(radius):
-    print(float(str))
+  def getCir(self):
+    return self.cir
 
-  def getCircufrence(circufrence):
-    return self.circufrence
-
-  def getArea(area):
+  def getArea(self):
     return self.area
     
-  def main():
-    rad = float(input("Enter the Radius: "))
-    fortnite = fortnite(rad)
-    fortnite.calculate()
-    print("Area: ", fortnite.getArea())
-    print("Circufrence: ", fortnite.getCircufrence())
+def main():
+   rad = float(input("Enter the Radius: "))
+   fortnit = fortnite(rad)
+   fortnit.calculate()
+   print("Area: ", round(fortnit.getArea(),2))
+   print("Circumfrence: ", round(fortnit.getCir(),2))
 
-    pass
+   pass
 
 
-  if __name__ == "__main__":
-    main()
+if __name__ == "__main__":
+  main()

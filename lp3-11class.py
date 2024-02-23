30e97f15c3e33544f4c72b121474e3c9f09a751b
jimmyarneson <143823450+jimmyarneson@users.noreply.github.com>
2024-02-23 03:30:28 +0000

    2/22/24

--- lp3-11class.py
+++ lp3-11class.py
@@ -1,11 +1,11 @@
 class forklift:
-  def __inti__(self, desinging, coding, debuging, testing):
+  def __init__(self, desinging, coding, debuging, testing):
     self.desinging = desinging
     self.coding = coding
     self.debuging = debuging
     self.testing = testing
     self._time = 0
-    self._percent = [0]*4
+    self._percents = [0]*4
 
   def __percent(self, number):
     return round((number/self._time)*100 , 2)
@@ -18,12 +18,12 @@ class forklift:
     self._percents[3] = self.__percent(self.testing)
 
   def display(self):
-    print("Category\t\tTime")
-    print(f"Desinging\t\t\t{self._percents[0]}%")
-    print(f"Coding\t\t{self._percents[1]}%")
-    print(f"Debuging\t{self._percents[2]}%")
-    print(f"Testing\t\t\t{self._percents[3]}%")
-    print(f"Total Time Spent:{self._budget:.2f}")
+    print("Category\t\t\tTime")
+    print(f"Desinging\t\t    {self._percents[0]}%")
+    print(f"Coding\t\t\t    {self._percents[1]}%")
+    print(f"Debuging\t\t    {self._percents[2]}%")
+    print(f"Testing\t\t\t    {self._percents[3]}%")
+    print(f"Total Time Spent: {self._time:.2f} miniutes")
 
 def main():
   print("Enter the Amount of Time Spent on the Following: ")
@@ -33,9 +33,9 @@ def main():
   testing = float(input("Testing: "))
   print()
 
-  time = forklift(desining, coding, debuging, testing)
-  time.calculate()
-  time.display
+  forkliftdriver = forklift(desining, coding, debuging, testing)
+  forkliftdriver.calculate()
+  forkliftdriver.display()
 
   pass
 

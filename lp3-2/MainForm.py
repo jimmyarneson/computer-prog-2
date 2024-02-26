15c47525d1369ce35cc8cdcfc45531fccc2abfbb
jimmyarneson <arneson.j1@stu.janesville.k12.wi.us>
2024-02-26 14:36:14 -0600

    2/26/24

---
+++ lp3-2/MainForm.py
@@ -0,0 +1,93 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._label1 = System.Windows.Forms.Label()
+		self._textBox1 = System.Windows.Forms.TextBox()
+		self._label2 = System.Windows.Forms.Label()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# label1
+		# 
+		self._label1.BackColor = System.Drawing.Color.Purple
+		self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(13, 13)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(345, 27)
+		self._label1.TabIndex = 0
+		self._label1.Text = "Enter the diamter of the pizza:"
+		# 
+		# textBox1
+		# 
+		self._textBox1.BackColor = System.Drawing.Color.Purple
+		self._textBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._textBox1.Location = System.Drawing.Point(13, 44)
+		self._textBox1.Name = "textBox1"
+		self._textBox1.Size = System.Drawing.Size(345, 29)
+		self._textBox1.TabIndex = 1
+		# 
+		# label2
+		# 
+		self._label2.BackColor = System.Drawing.Color.Purple
+		self._label2.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label2.Location = System.Drawing.Point(13, 80)
+		self._label2.Name = "label2"
+		self._label2.Size = System.Drawing.Size(345, 73)
+		self._label2.TabIndex = 2
+		self._label2.Text = "label2"
+		# 
+		# button1
+		# 
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(13, 157)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(114, 39)
+		self._button1.TabIndex = 3
+		self._button1.Text = "Calculate"
+		self._button1.UseVisualStyleBackColor = True
+		# 
+		# button2
+		# 
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(124, 156)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(125, 39)
+		self._button2.TabIndex = 4
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = True
+		# 
+		# button3
+		# 
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(244, 156)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(114, 39)
+		self._button3.TabIndex = 5
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = True
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.Yellow
+		self.ClientSize = System.Drawing.Size(370, 205)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._label2)
+		self.Controls.Add(self._textBox1)
+		self.Controls.Add(self._label1)
+		self.Name = "MainForm"
+		self.Text = "lp3-2"
+		self.ResumeLayout(False)
+		self.PerformLayout()
+

8afc478ae3d73ecc980ee4a3fcd08d592ebf092b
jimmyarneson <143823450+jimmyarneson@users.noreply.github.com>
2024-03-14 19:28:16 +0000

    2/14/24

--- CL213f.py
+++ CL213f.py
@@ -4,12 +4,13 @@ class responsibility:
     self._cost = 0
 
   def calc(self):
-    if 0 < self.kwh <= 2000:
-      self._cost += 0.07 
-    elif 2000 < self.kwh <= 8000:
-      self._cost += 0.05 
+    if 0 <= self.kwh <= 2000:
+      self._cost += 0.07 * self.kwh
+    elif 2000 < self.kwh <= 9999:
+      self._cost += 2000 * 0.07 + (self.kwh -2000) * 0.05 
     elif self.kwh > 10000:
-      self._cost += 0.04 
+      self._cost += 2000 * 0.07 + 8000 * 0.05 + (self.kwh -10000) * 0.04
+    
 
   def __str__(self):
-    return f"The cost of {self.kwh} is {self._cost}"
+    return f"The cost of {self.kwh} is {round(self._cost , 2)}"

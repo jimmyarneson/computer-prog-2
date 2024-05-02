688871ae0d995a0bd6e6359c960327eaf80d0d6c
jimmyarneson <arneson.j1@stu.janesville.k12.wi.us>
2024-05-02 14:35:28 -0500

    5/2/24

--- CSharp/CSharp/lang122bWhile/Form1.cs
+++ CSharp/CSharp/lang122bWhile/Form1.cs
@@ -9,9 +9,23 @@ namespace lang122bWhile
 
         private void button1_Click(object sender, EventArgs e)
         {
-            while (true){
-
+            listBox1.Items.Add("Hours\t\tPay");
+            int lcv = 1;
+            while(lcv <= 40) {
+                int pay = (int)lcv * 4;
+                listBox1.Items.Add(lcv + "\t\t" + pay);
+                lcv++;
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }

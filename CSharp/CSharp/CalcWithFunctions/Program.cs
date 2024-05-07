076fbf8ea1d62c123e61240b67b9e861b561dd7e
jimmyarneson <arneson.j1@stu.janesville.k12.wi.us>
2024-05-07 14:30:33 -0500

    5/7/24

---
+++ CSharp/CSharp/CalcWithFunctions/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Enter Num1: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Enter Num2: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Choose an option: add, sub, mul, or div: ");
+string op = Console.ReadLine();
+int result = 0;
+
+if (op == "add") result =  add(num1, num2);
+else if (op == "sub") result = sub(num1, num2);
+else if (op == "mul") result = mul(num1, num2);
+else if (op == "div") result = div(num1, num2);
+Console.WriteLine("Result: " + result);
+wait();
+
+static void wait() {  //void means "return nothing"
+    Console.ReadLine();
+    // return;
+}
+
+static int add(int x, int y) {
+    return x + y;
+}
+
+static int sub(int x, int y) {
+    return x - y;
+}
+
+static int mul(int x, int y) {
+    return x * y;
+}
+
+static int div(int x, int y) {
+    return x / y;
+}
